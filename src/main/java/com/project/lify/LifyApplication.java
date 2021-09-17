4c0cf62545dbae2d3e0af708f3b69e423d3bb4bb
keepbang <rlqud1125@gmail.com>
2021-09-17 21:43:35 +0900

    create spring

---
+++ src/main/java/com/project/lify/LifyApplication.java
@@ -0,0 +1,13 @@
+package com.project.lify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/lify/LifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
