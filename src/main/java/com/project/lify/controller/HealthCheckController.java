06a9256f1cb526b8915425c5ad863cdd17cce7cf
keepbang <rlqud1125@gmail.com>
2021-09-26 20:15:32 +0900

    Feat: Create HealthCheck Api

---
+++ src/main/java/com/project/lify/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.project.lify.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author keepbang
+ */
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/api/ping")
+    public String pint(){
+        return "ok";
+    }
+}
